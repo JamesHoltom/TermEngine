1323de8a19eaacbe8296cfa03d938abfc681000f
JamesHoltom <james_holtom@hotmail.co.uk>
2022-03-22 22:51:02 +0000

    Update lua files

--- lua/noprogram.lua
+++ lua/noprogram.lua
@@ -1,12 +1,18 @@
 local default_text_color = vec3(255)
 local default_bg_color = vec3(0)
 
+local outline, noProgramText
+
 function Init()
 	print("Init -> Init()")
 	
-	addOutlinedBox("no_prg_outline", vec2(0), ivec2(32, 16), Glyph("#", default_text_color, default_bg_color))
-	addText("no_prg_text", vec2(11, 7), ivec2(21, 7), "No Program", default_text_color, default_bg_color)
-	
+	outline = BoxObject(vec2(0), ivec2(32, 16))
+	outline.setOutline(Glyph("#", default_text_color, default_bg_color))
+
+	noProgramText = TextObject(vec2(11, 7), ivec2(21, 7))
+	noProgramText.setText("No Program")
+	noProgramText.setColors(default_text_color, vec3(255, 0, 0))
+
 	return true
 end
 
